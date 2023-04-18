d3792069347b12cffa4efad7c8831a1629544f93
Adaeze06 <adaezefredericks01@gmail.com>
2023-04-17 22:44:32 -0400

    Base

--- main.py
+++ main.py
@@ -20,13 +20,29 @@ logo_label.grid(column=1, row=0)
 instructions = tk.Label(root, text = "Welcome to Elite Savings!  Enter your Account Number to log in.")
 instructions.grid(columnspan=3, column=0, row=1)
 
-#buttons
+#function for Button
+
+
+
+
+#register button
 register_text=tk.StringVar()
-register_btn=tk.Button(root, textvariable= register_text, bg= '#FF99AC', fg='white', height=2, width=15)
+register_btn=tk.Button(root, textvariable= register_text, bg= '#fc9484', fg='white', height=2, width=15)
 register_text.set('Register')
 register_btn.grid(column=1, row=2)
 
-canvas = tk.Canvas(root, height=2500, width=600)
+canvas = tk.Canvas(root, height=250, width=600)
 canvas.grid(columnspan=3)
+
+#login button
+login_text=tk.StringVar()
+login_btn=tk.Button(root, textvariable= login_text, bg= '#fc9484', fg='white', height=2, width=15)
+login_text.set('Login')
+login_btn.grid(column=1, row=3)
+
+canvas = tk.Canvas(root, height=250, width=600)
+canvas.grid(columnspan=3)
+
+
 
 root.mainloop()
