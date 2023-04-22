3ad089c2e8b97e9e9ddfb30864b1c37a2a450006
Adaeze06 <adaezefredericks01@gmail.com>
2023-04-22 18:25:25 -0400

    Update

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 
 root = tk.Tk()
 
-
 #window setup
 canvas = tk.Canvas(root, height=300, width=600)
 canvas.grid(columnspan=3, rowspan=3)
@@ -20,29 +19,25 @@ logo_label.grid(column=1, row=0)
 instructions = tk.Label(root, text = "Welcome to Elite Savings!  Enter your Account Number to log in.")
 instructions.grid(columnspan=3, column=0, row=1)
 
-#function for Button
 
-
-
-
-#register button
-register_text=tk.StringVar()
-register_btn=tk.Button(root, textvariable= register_text, bg= '#fc9484', fg='white', height=2, width=15)
-register_text.set('Register')
-register_btn.grid(column=1, row=2)
-
-canvas = tk.Canvas(root, height=250, width=600)
-canvas.grid(columnspan=3)
+#function for Registering New User
+def create_account():
+    print('is this working')
 
 #login button
 login_text=tk.StringVar()
 login_btn=tk.Button(root, textvariable= login_text, bg= '#fc9484', fg='white', height=2, width=15)
 login_text.set('Login')
-login_btn.grid(column=1, row=3)
+login_btn.grid(column=1, row=2)
 
-canvas = tk.Canvas(root, height=250, width=600)
+#register button
+register_text=tk.StringVar()
+register_btn=tk.Button(root, textvariable= register_text, command=lambda:create_account(), bg= '#fc9484', fg='white', height=2, width=15)
+register_text.set('Create Account')
+register_btn.grid(column=1, row=3)
+
+canvas = tk.Canvas(root, height=200, width=100)
 canvas.grid(columnspan=3)
-
 
 
 root.mainloop()
